aa19097a463190059459216c9e1910a74c2bb991
Nipun Mittal <nipunmittal15@gmail.com>
2020-05-05 22:17:56 +0530

    problem_317 updated

--- solutions/problem_317.cpp
+++ solutions/problem_317.cpp
@@ -10,12 +10,15 @@ int main()
     cin >> n;
     int i;
     // assuming 32 bit INTEGER
+    // Find first MSB in "M" and "N" which is not same
+    // it implies that after this bit position both "0" and "1" bits are possible from M to N. So all bits in answer after that will be "0".
     for(i = 31; i >= 0; --i) {
         if(((n >> i)&1) != ((m >> i)&1)) {
             break;
         }
     }
     ++i;
+    // answer is all the bits till "i" and all "0" after that
     int ans = (n >> i) << i;
     cout << "Birwise AND of [M, N] is: " << ans << endl;
     return 0;
